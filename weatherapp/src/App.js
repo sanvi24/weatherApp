8884878050f5c40a9a510aefdaf6b2d21ae6bef5
sanvi24 <sandhyarani813078@gmail.com>
2024-04-26 12:00:37 +0530

    Minor fix

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,13 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getLatAndLong } from "./helpers/getLatAndLong";
-import { getWeatherDetails } from "./weather";
-import Dashboard from "./components/Dashboard";
+import React from "react";
+import Dashboard from "./components/Dashboard"
 
 function App() {
   return (
     <div className="bg-[#1E1E1E] min-w-sm min-h-screen">
-      <Dashboard />
+     <Dashboard />
     </div>
   )
 }
